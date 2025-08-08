82bcf3c1628a777d073896a17bc9b13b67d2267b
Pedro Marques <pedro.marques3005@live.com>
2025-08-07 22:46:16 -0300

    Atualiza o menu

--- src/app/funcionalidades/menu/page.js
+++ src/app/funcionalidades/menu/page.js
@@ -1,5 +1,5 @@
-"use client"
-import { Box, Flex, Text, Link, Button, Image } from "@chakra-ui/react"
+"use client";
+import { Box, Flex, Text, Link, Button, Image } from "@chakra-ui/react";
 
 export default function Menu() {
   return (
@@ -11,19 +11,63 @@ export default function Menu() {
       width={"100vw"}
       scrollWidth={"0"}
       color={"#000"}
+      gap={10}
     >
       <Box>
         <Text mt={20}>Menu</Text>
       </Box>
-      <Box>
-        <Link> </Link>
-      </Box>
-      <Box>
-        <Link> </Link>
-      </Box>
-      <Box>
-        <Link> </Link>
+      <Box color={"black"} fontSize={"xl"} textAlign={"center"} gap={10}>
+        <Box
+          p={2}
+          px={100}
+          m={2}
+          backdropBlur={"10px"}
+          bg={"teal.800"}
+          borderRadius={10}
+          shadow={"md"}
+          _hover={{ bg: "teal.600", color: "white", cursor: "pointer" }}
+        >
+          <Link color={"whiteAlpha.800"}> CADASTRO DE ITEM </Link>
+        </Box>
+        <Box
+          p={2}
+          m={2}
+          backdropBlur={"10px"}
+          bg={"teal.800"}
+          borderRadius={10}
+          shadow={"md"}
+          _hover={{ bg: "teal.600", color: "white", cursor: "pointer" }}
+        >
+          <Link color={"whiteAlpha.800"}> EXIBIÇÃO DE ESTOQUE </Link>
+        </Box>
+        <Box
+          p={2}
+          m={2}
+          backdropBlur={"10px"}
+          bg={"teal.800"}
+          borderRadius={10}
+          shadow={"md"}
+          _hover={{
+            bg: "teal.600",
+            color: "white",
+            cursor: "pointer",
+            transitionDuration: "0.9s",
+          }}
+        >
+          <Link color={"whiteAlpha.800"}> ENTRADA E SAÍDA </Link>
+        </Box>
+        <Box
+          p={2}
+          m={2}
+          backdropBlur={"10px"}
+          bg={"teal.800"}
+          borderRadius={10}
+          shadow={"md"}
+          _hover={{ bg: "teal.600", color: "white", cursor: "pointer" }}
+        >
+          <Link color={"whiteAlpha.800"}> RELATÓRIOS </Link>
+        </Box>
       </Box>
     </Flex>
-  )
+  );
 }
