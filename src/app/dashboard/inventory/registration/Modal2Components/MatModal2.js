19d4a9dd0a05caa11aeff82e90d7fa7bcd4e1387
Pedro Marques <pedro.marques3005@live.com>
2025-08-24 18:58:25 -0300

    Adiciona regras ao MatModal2

--- src/app/dashboard/inventory/registration/Modal2Components/MatModal2.js
+++ src/app/dashboard/inventory/registration/Modal2Components/MatModal2.js
@@ -1,6 +1,14 @@
 "use client";
 import React from "react";
-import { Flex, Input, Text, Box, Field, Switch } from "@chakra-ui/react";
+import {
+  Flex,
+  Input,
+  Text,
+  Box,
+  Field,
+  Switch,
+  NativeSelect,
+} from "@chakra-ui/react";
 import { FaSyringe } from "react-icons/fa";
 
 export default function MatModal2({ data, setData }) {
@@ -19,17 +27,11 @@ export default function MatModal2({ data, setData }) {
           Classificação
         </Text>
       </Flex>
-      <Flex
-        border="1px solid green"
-        p={4}
-        gap={4}
-        alignItems={"center"}
-        justifyContent={"center"}
-      >
+      <Flex p={4} gap={4} alignItems={"center"} justifyContent={"center"}>
         <Field.Root>
           <Flex alignItems="center" gap={4} justifyContent={"center"}>
             <Field.Label fontSize="sm" fontWeight="bold" color="gray.700">
-              Contém registro Anvisa
+              Contém registro completo na Anvisa
             </Field.Label>
             <Switch.Root
               checked={data.contemRegistroAnvisa}
@@ -48,14 +50,11 @@ export default function MatModal2({ data, setData }) {
         </Field.Root>
       </Flex>
       {data.contemRegistroAnvisa && (
-        <Flex
-          p={4}
-          gap={4}
-          justifyContent="space-between"
-          border={"1px solid red"}
-        >
+        <Flex p={4} gap={4} justifyContent="space-between">
           <Field.Root flex="1" minW="0">
-            <Field.Label fontWeight="semibold">Registro ANVISA</Field.Label>
+            <Field.Label fontSize="sm" fontWeight="bold" color="gray.700">
+              Registro ANVISA
+            </Field.Label>
             <Input
               placeholder="Digite o registro"
               maxLength={11}
@@ -66,55 +65,94 @@ export default function MatModal2({ data, setData }) {
             />
           </Field.Root>
 
+          <Box flex="1" minW="0"></Box>
+        </Flex>
+      )}
+
+      <Flex p={4} gap={4} alignItems={"center"} justifyContent={"center"}>
+        <Field.Root>
+          <Flex alignItems="center" gap={4} justifyContent={"center"}>
+            <Field.Label fontSize="sm" fontWeight="bold" color="gray.700">
+              Sujeito a notificação simplificada
+            </Field.Label>
+            <Switch.Root
+              checked={data.refNormativaNotificacaoSimplificada}
+              onCheckedChange={(e) =>
+                setData({
+                  ...data,
+                  refNormativaNotificacaoSimplificada: e.checked,
+                })
+              }
+              colorPalette="blue"
+              size="lg"
+            >
+              <Switch.HiddenInput />
+              <Switch.Control>
+                <Switch.Thumb />
+              </Switch.Control>
+            </Switch.Root>
+          </Flex>
+        </Field.Root>
+      </Flex>
+      {data.refNormativaNotificacaoSimplificada && (
+        <Flex p={4} gap={4} justifyContent="space-between">
           <Field.Root flex="1" minW="0">
-            <Field.Label fontWeight="semibold">
-              Validade do Registro
+            <Field.Label fontSize="sm" fontWeight="bold" color="gray.700">
+              Referência normativa (RDC aplicável)
             </Field.Label>
             <Input
-              type="date"
+              placeholder="RDC xx/xxxx"
               bg="white"
               boxShadow="md"
               border="1px solid #2b4d52ff"
               _hover={{ borderColor: "#5d8288c4" }}
             />
           </Field.Root>
+
+          <Box flex="1" minW="0"></Box>
         </Flex>
       )}
 
       <Flex p={4} gap={4} justifyContent="space-between">
         <Field.Root flex="1" minW="0">
-          <Field.Label fontWeight="semibold">Classe farmacêutica</Field.Label>
-          <Input
-            placeholder="Insira a classe"
-            bg="white"
-            boxShadow="md"
-            border="1px solid #2b4d52ff"
-            _hover={{ borderColor: "#5d8288c4" }}
-          />
+          <Field.Label fontSize="sm" fontWeight="bold" color="gray.700">
+            Classificação de Risco
+          </Field.Label>
+          <NativeSelect.Root>
+            <NativeSelect.Field
+              unstyled
+              value={data.classificacaoDeRisco}
+              onChange={(e) =>
+                setData({ ...data, classificacaoDeRisco: e.target.value })
+              }
+              width="100%"
+              bg="white"
+              boxShadow="md"
+              color="black"
+              borderRadius="md"
+              border="1px solid #2b4d52ff"
+              px={3}
+              py={2}
+              _hover={{
+                borderColor: "#5d8288c4",
+              }}
+            >
+              <option value={""}>Selecione</option>
+              <option value={"Classe I"}>Classe I (Baixo Risco)</option>
+              <option value={"Classe II"}>Classe II (Médio Risco)</option>
+              <option value={"Classe III"}>Classe III (Alto Risco)</option>
+              <option value={"Classe IV"}>Classe IV (Máximo Risco)</option>
+            </NativeSelect.Field>
+          </NativeSelect.Root>
         </Field.Root>
 
-        <Field.Root flex="1" minW="0">
-          <Field.Label>Validade total do medicamento: </Field.Label>
-          <Input
-            bg="white"
-            boxShadow="md"
-            border="1px solid #2b4d52ff"
-            _hover={{ borderColor: "#5d8288c4" }}
-          />
-        </Field.Root>
+        <Box flex="1" minW="0"></Box>
       </Flex>
-
-      <Flex p={4}>
-        <Field.Root width="100%">
-          <Field.Label fontWeight="semibold">Forma farmacêutica</Field.Label>
-          <Input
-            placeholder="Insira a forma farmacêutica"
-            bg="white"
-            boxShadow="md"
-            border="1px solid #2b4d52ff"
-            _hover={{ borderColor: "#5d8288c4" }}
-            maxWidth="500px"
-          />
+      <Flex p={4} gap={4} justifyContent="space-between">
+        <Field.Root>
+          <Field.Label fontSize="sm" fontWeight="bold" color="gray.700">
+            Composição principal
+          </Field.Label>
         </Field.Root>
       </Flex>
     </Flex>
